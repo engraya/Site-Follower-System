f52a7822b291ad0ec1b782f34da05530d9e86724
EngrAhmad <ahmadyakubuaya@gmail.com>
2023-05-07 14:26:07 +0100

    Commit

--- base/forms.py
+++ base/forms.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 class CustomUserForm(UserCreationForm):
     email = forms.EmailField(required=False)
 
-
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
